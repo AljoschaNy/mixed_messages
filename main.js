e4628ea88e093651beb15447d7e7e80c723b5339
aljoschaN91 <70570811+AljoschaNy@users.noreply.github.com>
2021-02-04 14:31:00 +0100

    Create different message parts

---
+++ main.js
@@ -0,0 +1,34 @@
+// Beginning message part
+const beginningMessage = [
+    'My tip to you my friend: ',
+    'My tip for today: ',
+    'My today\'s advice: '
+];
+// Main message part
+const mainMessage = [
+    'Make today count. ',
+    'Be the best YOU of all time. ',
+    'You can make it happen if you really want to. ',
+    'Work hard every single day. ',
+    'Don\'t let someone else decide what\'s best for you. ',
+    'Keep going unless you get it and beyond. ',
+    'Don\'t stop because it is geting harder. '
+];
+// Ending message part
+const endMessage = [
+    'Have a nice day!',
+    'Keep on hustling!',
+    'Keep on learning!',
+    'I believe in you!',
+    'You\'re the best!',
+    'And now make it happen!'
+];
+
+
+
+// Testing
+//console.log(beginningMessage.length);
+//console.log(mainMessage.length);
+//console.log(endMessage.length);
+
+//console.log(beginningMessage[0]+mainMessage[0]+endMessage[0]);
